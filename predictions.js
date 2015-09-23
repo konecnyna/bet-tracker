dd3c5e0bba2ae3ff418f4bb063b91bea05d0667c
nkonecny <nkonecny@etsy.com>
2015-09-23 14:16:32 -0400

    Fixed predictions

--- predictions.js
+++ predictions.js
@@ -3,8 +3,10 @@ var util = require('util');
 var async = require('async');
 var fs = require('fs');
 var path = require('path');
-var positions = ["QB", "RB", "WR", "K"]; //, "TE"
+var positions = ["QB", "RB", "WR", "K", "TE"]; //, "TE"
 var base_url = "http://www.fantasyfootballnerd.com/service/weekly-projections/json/test/%s/3/";
+var schedule_url = "http://www.fantasyfootballnerd.com/service/schedule/json/ejwqdwezs7xi/";
+var DEBUG = true;
 module.exports = {
   getScores: function (callback) {
   	getScores(callback);
@@ -25,10 +27,52 @@ function getScores(callback){
 		});
 
 	}, function done() {
-		callback(gameScores);
-	  
+		//callback(gameScores);
+	  	utils.downloadFile("http://www.fantasyfootballnerd.com/service/weekly-projections/json/ejwqdwezs7xi/def/3/", function(data){
+			var games = JSON.parse(data);
+			generateScores(games.Projections, gameScores);
+			getScheduleAndApplyScores(callback, gameScores);
+		});
+	  	
 	});
 	
+}
+
+function getScheduleAndApplyScores(callback, gameScores){
+  	utils.downloadFile(schedule_url, function(data){
+		var games = JSON.parse(data);
+		var currentWeek = DEBUG ? '2' : games.currentWeek;
+
+		var currentWeekGames = []
+		for(var i=0; i<games.Schedule.length; i++){
+			var currentGame = games.Schedule[i];
+			if(currentGame.gameWeek === currentWeek){
+				currentGame.awayTeamPredicition = gameScores[currentGame.awayTeam];
+				currentGame.homeTeamPredicition = gameScores[currentGame.homeTeam] + 17;
+				
+				if(DEBUG){
+					if(currentGame.winner === currentGame.awayTeam){
+						currentGame.correct_predict = (currentGame.awayTeamPredicition - currentGame.homeTeamPredicition) > 0;
+					}else{
+						currentGame.correct_predict = (currentGame.homeTeamPredicition - currentGame.awayTeamPredicition) > 0;
+					}
+					
+					if(!currentGame.correct_predict){
+						currentWeekGames.push(currentGame);
+					}
+
+				}else{
+					currentWeekGames.push(currentGame);	
+				}			
+			}
+		}
+
+		if(DEBUG){
+			console.log("NUMBER WRONG: " + currentWeekGames.length + "/16");			
+		}
+		callback(currentWeekGames);
+	});
+
 }
 
 function generateScores(games, gamesScores){
@@ -36,18 +80,21 @@ function generateScores(games, gamesScores){
 		score = 0;
 		score += parseInt(games[i].rushTD) * 7;
 		score += parseInt(games[i].passTD) * 7;
-		score += parseInt(games[i].rushYds) / 10;
-		score += parseInt(games[i].passYds) / 100;
+		//score += parseInt(games[i].rushYds) / 10;
+		//score += parseInt(games[i].passYds) / 100;
 
 		if(games[i].recYds){
-			score += parseInt(games[i].recYds) / 10;
+			//score += parseInt(games[i].recYds) / 10;
 			score += parseInt(games[i].recTD) * 7;
 		}
 
 		if(games[i].fg){
 			score += games[i].fg * 3;
 		}
-			
+		
+		if(games[i].defTD){
+			score += games[i].defTD * 7;
+		}
 		
 		if(gamesScores[games[i].team]){
 			gamesScores[games[i].team] = score + gamesScores[games[i].team];					
