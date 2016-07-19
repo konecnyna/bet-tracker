8f1371ffcedf638ee62af64386680693de870143
nkonecny <nkonecny@etsy.com>
2016-07-19 11:29:22 -0400

    twerking modules

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 var express = require('express');
+var fs = require('fs');
+var SportsStream = require('sport-streams');
 var server = require('./lib/server.js');
-
 var app = module.exports = express();
-var betTracker = new server(app);
 
-app.listen(9000);
+var credsFile = "creds.dat";
+
+
+// Authenticator
+fs.readFile(credsFile, 'utf8', function (err,password) {
+	if (err) {
+	  console.log("Error - no creds.dat file so no auth!");
+	} else {
+	  console.log("asking for auth!");
+	  app.use(function(req, res, next) {
+	     var auth;
+	     if (req.headers.authorization) {
+	        auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+	      }
+	      if (!auth || auth[0] !== 'admin' || auth[1] !== password.trim()) {
+	          res.statusCode = 401;
+	          res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+	          res.end('Unauthorized');
+	      } else {
+	          next();
+	      }
+	  });
+	}
+
+
+	//Modules.
+	var sport = new SportsStream(app);
+	var betTracker = new server(app);	
+	
+	app.listen(9000);
+
+});
+
+
 
 
 
