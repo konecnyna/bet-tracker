d789b05a63f53985035e9cd553710c1fabef9701
Nick Konecny <nkonecny@stashinvest.com>
2019-09-19 12:35:26 -0400

    updoot

--- analyzer/generic-algo/fitness.js
+++ analyzer/generic-algo/fitness.js
@@ -37,12 +37,21 @@ module.exports = class Fitness {
 
   confidence(game, genes, result, analysts_overall) {
     const confidence = {};
-    const picks = {};
     confidence[game.result.homeTeam] = 0;
     confidence[game.result.awayTeam] = 0;
-    picks[game.result.homeTeam] = 0;
-    picks[game.result.awayTeam] = 0;
+    this.addAnalystScore(game, genes, confidence)
+    this.addHomeFieldAdvantage(game, genes, confidence)
+    return confidence;
+  }
 
+  addHomeFieldAdvantage(game, genes, confidence) {
+    confidence[game.result.homeTeam] += genes[Chromosome.homeFieldAdvantageGeneIndex] * .1
+  }
+
+  addAnalystScore(game, genes, confidence) {
+    const analystPicks = {};
+    analystPicks[game.result.homeTeam] = 0;
+    analystPicks[game.result.awayTeam] = 0;
     game.picks.map((pick, i) => {
       if (i > Chromosome.analystEndIndex) {
         return;
@@ -55,22 +64,19 @@ module.exports = class Fitness {
       } else {
         confidence[pick] += rating;
       }
-      
       confidence[pick] += analysts_overall[i] * .5;
+      confidence[game.result.homeTeam] += genes[Chromosome.homeFieldAdvantageGeneIndex] * 0.1;
       picks[pick] += 1;
     });
 
-    confidence[game.result.homeTeam] +=
-      genes[Chromosome.homeFieldAdvantageGeneIndex] * 0.1;
-
-    Object.keys(picks).map(key => {
-      if (picks[key] === 0) {
+    Object.keys(analystPicks).map(key => {
+      if (analystPicks[key] === 0) {
         return;
       }
 
-      confidence[key] = confidence[key] / picks[key];
+      confidence[key] = confidence[key] / analystPicks[key];
     });
 
-    return confidence;
+    return analystPicks;
   }
 };
