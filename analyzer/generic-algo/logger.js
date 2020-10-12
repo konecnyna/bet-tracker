768042ac11032bdb2560da756bb0743c11f7072b
konecnyna <konecnyna@gmail.com>
2020-10-12 17:05:45 -0400

    [FIXED] FINALLT

--- analyzer/generic-algo/logger.js
+++
@@ -1,7 +0,0 @@
-module.exports.dumpAlogInfo = geneticAlgorithm => {
-  const best = geneticAlgorithm.best();
-  const score = geneticAlgorithm.bestScore();
-  console.log("Finished with:");
-  console.log("Model:", best.chromosome.genes);
-  console.log(`Final confidence: ${score}`);
-};
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var path = require("path");
 var games = require("./lib/games.js");
-var predictions = require("./lib/predictions.js");
 var rss = require("./lib/football_rss.js");
 const request = require('request-promise');
 var path = require("path");
+const { select } = require("async");
 var ROOT_NAME = "/bet-tracker";
 
 const URL = `https://project-3654207232474154346.firebaseio.com/bet-tracker/games.json`
@@ -18,55 +18,49 @@ function BetTracker(app) {
 
   app.get(ROOT_NAME + "/api/v1/scores", async (req, res) => {
     const options = {
-      url: URL,
+      url: "http://home-remote-api.herokuapp.com/bets/open",
       json: true,
     };
     const body = await request.get(options)
-
+    const bets = body || [];
     games.getUIData(function (callback) {
       res.json(callback);
-    }, body);
+    }, bets.map(({ pick, spread }) => {
+      if (!pick || !spread) { return null }
+      const split = pick.split(" ");
+      const bet = {}
+      bet[split[split.length - 1]] = {
+        spread: spread
+      }
+      return [bet];
+    }).filter(it => it));
   });
 
   app.get(ROOT_NAME + "/api/v1/picks", async (req, res) => {
     const options = {
-      url: URL,
+      url: "http://home-remote-api.herokuapp.com/bets/open",
       json: true,
     };
     const body = await request.get(options)
     var prettyJson = JSON.stringify(body, null, 4);
-    res.json(prettyJson);
+    res.json([{ "Seahawks": { "spread": 6 } }]);
   });
 
-
-  app.get(ROOT_NAME + "/api/v1/predictions", function (req, res) {
-    predictions.getScores(function (callback) {
-      res.json(callback);
-    });
-  });
-
-  app.get(ROOT_NAME + "/api/v1/update_picks", async (req, res) => {
-    try {
-      const picks = JSON.parse(req.query.picks);
-      const options = {
-        url: URL,
-        json: true,
-        body: picks,
-      };
-      const body = await request.put(options);
-      res.json(body);
-    } catch (e) {
-      console.log("Bad json: " + e);
-      res.send("fail!");
-    }
-  });
-
-  app.get(ROOT_NAME + "/api/v1/rss", function (req, res) {
-    rss.getFootballRss(function (xml) {
-      res.set("Content-Type", "text/xml");
-      res.send(xml);
-    });
-  });
+  // app.get(ROOT_NAME + "/api/v1/update_picks", async (req, res) => {
+  //   try {
+  //     const picks = JSON.parse(req.query.picks);
+  //     const options = {
+  //       url: URL,
+  //       json: true,
+  //       body: picks,
+  //     };
+  //     const body = await request.put(options);
+  //     res.json(body);
+  //   } catch (e) {
+  //     console.log("Bad json: " + e);
+  //     res.send("fail!");
+  //   }
+  // });
 }
 
 module.exports = BetTracker;
